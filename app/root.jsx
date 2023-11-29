aa39b833ef29a05c70df11265c53ce316116177a
Alok Swamy <alok.swamy@shopify.com>
2023-11-29 15:14:14 -0500

    Update root.jsx

--- app/root.jsx
+++ app/root.jsx
@@ -118,7 +118,7 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <h2>Moo Moo Meadows</h2>
+        <h2>Dry dry ruins</h2>
         <Layout {...data}>
           <Outlet />
         </Layout>
