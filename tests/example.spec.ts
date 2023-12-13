cfe2c8f44b838fca855ae47f6842ca62217b9b8d
Alok Swamy <alok.swamy@shopify.com>
2023-12-13 14:01:06 -0500

    Update e2e tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -14,5 +14,5 @@ test('page shows shop name', async ({page}) => {
   await page.goto(process.env.DEPLOYMENT_URL);
 
   await expect(page).toHaveTitle(/Hydrogen | Home/);
-  await expect(page.getByText('Alok Shop Exp')).toBeVisible();
+  await expect(page.getByText('bottles of sand')).toBeVisible();
 });
