6fc9aff003ceaaccb5fc06487c2b55b87b855c25
Alok Swamy <alok.swamy@shopify.com>
2024-03-07 18:45:06 -0500

    Move end-to-end tests into a separate GitHub Action (#5)

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
-import {defineConfig, devices} from '@playwright/test';
+import {
+  defineConfig,
+  devices,
+  type PlaywrightTestConfig,
+} from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -9,7 +13,7 @@ import {defineConfig, devices} from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
-export default defineConfig({
+let config: PlaywrightTestConfig = defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -23,9 +27,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
+    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+    actionTimeout: 0,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
@@ -38,26 +41,38 @@ export default defineConfig({
         ...devices['Desktop Chrome'],
       },
     },
-
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
+
+if (process.env.URL) {
+  const use = {
+    ...config.use,
+    baseURL: process.env.URL,
+  };
+
+  if (process.env.AUTH_BYPASS_TOKEN) {
+    use.extraHTTPHeaders = {
+      'oxygen-auth-bypass-token': process.env.AUTH_BYPASS_TOKEN,
+    };
+  }
+
+  config = {
+    ...config,
+    use,
+  };
+} else {
+  config = {
+    ...config,
+    use: {
+      ...config.use,
+      baseURL: 'http://localhost:3000',
+    },
+    /* Run your local dev server before starting the tests */
+    webServer: {
+      command: 'npm run preview',
+      port: 3000,
+    },
+  };
+}
+
+export default config;
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,17 +1,7 @@
 import {test, expect} from '@playwright/test';
 
 test('page shows shop name', async ({page}) => {
-  if (!process.env.DEPLOYMENT_URL) {
-    throw Error('DEPLOYMENT_URL is not set');
-  }
-
-  if (process.env.AUTH_BYPASS_TOKEN) {
-    page.setExtraHTTPHeaders({
-      'oxygen-auth-bypass-token': process.env.AUTH_BYPASS_TOKEN,
-    });
-  }
-
-  await page.goto(process.env.DEPLOYMENT_URL);
+  await page.goto('/');
 
   await expect(page).toHaveTitle(/Hydrogen | Home/);
   await expect(page.getByText('bottles of sand')).toBeVisible();
